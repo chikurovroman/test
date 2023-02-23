89f277fe918a351c057009cf8c29f96f70fa504f
chikurovroman <chikur1824@mail.ru>
2023-02-23 16:43:27 +0400

    add new field and javascript file

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function clock() {
     var seconds = d.getSeconds();
     var day_num = d.getDay();
     
-    month=["января", "февраля", "марта", "апреля", "мая", "июня",
-    "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+    month=["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
     weekday=["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]; 
+	
     if (day <= 9) day = "0" + day;
     if (hours <= 9) hours = "0" + hours;
     if (minutes <= 9) minutes = "0" + minutes;
-    date_time = "\n" + "Сегодня - " + day + " " + month[month_num] + " " + d.getFullYear() +
-    " г.&nbsp;&nbsp;&nbsp;Текущее время - "+ hours + ":" + minutes + ":" + seconds + " " + weekday[day_num] ; 
+	
+    date_time = "\n" + "Сегодня - " + day + " " + month[month_num] + " " + d.getFullYear() + " г.&nbsp;&nbsp;&nbsp;Текущее время - "+ hours + ":" + minutes + ":" + seconds + " " + weekday[day_num] ; 
     if (document.layers) {
     document.layers.doc_time.document.write(date_time);
     document.layers.doc_time.document.close();
